d264fa633caf061d6a785529523400633c608ca2
Imad-Ait-Ben-Salah <159795136+Imad-Ait-Ben-Salah@users.noreply.github.com>
2025-06-02 17:11:03 +0200

    fixes

--- fr.cs.groupF.myFoodora/src/exceptions/BadMealCompositionCreationException.java
+++ fr.cs.groupF.myFoodora/src/exceptions/BadMealCompositionCreationException.java
@@ -1,12 +1,21 @@
 package exceptions;
 
-public class BadMealCompositionCreationException extends Exception {
-    
-    public BadMealCompositionCreationException() {
-    	super();
-    }
 
-    public BadMealCompositionCreationException(String message) {
-        super(message);
-    } 
+@SuppressWarnings("serial")
+public class BadMealCompositionCreationException extends Exception{
+	public BadMealCompositionCreationException() {
+		super();
+	}
+	
+	public BadMealCompositionCreationException(String message) {
+		super(message);
+		
+	}
+	public BadMealCompositionCreationException(String message , Throwable cause) {
+		super(message,cause);
+		
+	}
+	public BadMealCompositionCreationException(Throwable cause) {
+		super(cause);
+	}
 }
